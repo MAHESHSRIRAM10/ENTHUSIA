b5afb84d0123086d77e99325212ebaa6baa75651
MAHESHSRIRAM10 <82818143+MAHESHSRIRAM10@users.noreply.github.com>
2022-01-15 22:56:18 +0530

    Update insertdata.php

--- insertdata.php
+++ insertdata.php
@@ -22,10 +22,10 @@ if(isset($_POST['submit'])){
 		{
 			echo "Form submitted successfully";
 		}
-        else
-        {
-            echo "Failed";
-        }
+                else
+                {
+                        echo "Failed";
+                }
 	
 }
 ?>
